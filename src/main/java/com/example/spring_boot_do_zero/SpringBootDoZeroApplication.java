602bd56ae713179a1442e7eb32747284b88e2e49
bandeira-th <bandeira.trabalho@gmail.com>
2024-07-04 17:33:28 -0300

    first commit

---
+++ src/main/java/com/example/spring_boot_do_zero/SpringBootDoZeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_do_zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDoZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDoZeroApplication.class, args);
+	}
+
+}
